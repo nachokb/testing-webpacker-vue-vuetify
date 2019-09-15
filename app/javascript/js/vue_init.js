6f70f6529d31369cd0c7f02ac2f898c76f85d903
Ignacio Carrera <icarrera@seiel.com.ar>
2019-09-15 05:04:40 -0300

    ref #2 try to import Vuetify

--- app/javascript/js/vue_init.js
+++ app/javascript/js/vue_init.js
@@ -1,5 +1,9 @@
 import Vue from 'vue';
 
+// vuetify
+import Vuetify from 'vuetify/lib';
+// Vue.use(Vuetify);
+
 // app's components
 import App from '../vue/app.vue';
 Vue.component('app', App);
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,13 @@
 const { environment } = require('@rails/webpacker')
+
 const { VueLoaderPlugin } = require('vue-loader')
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
 const vue = require('./loaders/vue')
 
+environment.plugins.prepend('VuetifyLoaderPlugin', new VuetifyLoaderPlugin())
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
-environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' };
+
+environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }
 
 module.exports = environment
